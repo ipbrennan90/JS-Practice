fbc72e3fbbf1539c8e53cf005fce369c44a33f6d
Ian Brennan <ian.brennan@icloud.com>
2015-04-04 14:50:45 -0600

    working on js stuff

---
+++ todo.js
@@ -0,0 +1,14 @@
+// var app = angular.module('todo', [ ]);
+
+(function(){
+    var app = angular.module('todo', [ ]);
+
+    app.controller('TodoController', function($scope){
+                $scope.myName= prompt("what's your name")
+                $scope.todo= {mostImportant:""};
+                $scope.color
+                $scope.color1
+                $scope.show_pic = true
+
+    });
+})();
